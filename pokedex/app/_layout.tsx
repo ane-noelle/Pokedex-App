33e99250409eb9ac500da0e257a38437d6c5af14
ane-noelle <anenoelle60@gmail.com>
2025-08-04 11:38:20 -0300

    dia2

--- pokedex/app/_layout.tsx
+++ pokedex/app/_layout.tsx
@@ -2,6 +2,6 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   <Stack>
-    <Stack.Screen name="index" options={{ headerShown: false}} />
+    <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
 }
